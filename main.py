3f9d737d40d9b372db053f30acfba25bd7e92471
Nightmare <nazarovdaniil05@gmail.com>
2025-02-22 19:30:59 +0300

    homework2

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 
+# #1
+# list1 = input().split()
+# list2 = []
+# for i in list1:
+#     if i not in list2:
+#          list2.append(i)
+# print(list2)
 
-list1 = input().split()
-list2 = []
-for i in list1:
-    if i not in list2:
-         list2.append(i)
-print(list2)
 
-list2 = []
-dict1 = {'a': 'abc', 'b': 'defgh', 'i': 'j', 'kl': 'mnopqr'}
+# #2
+# list2 = []
+# dict1 = {'a': 'abc', 'b': 'defgh', 'i': 'j', 'kl': 'mnopqr'}
 
-max_length = 0
-max_key = ''
-for key, value in dict1.items():
-        current_length = len(value)  
-        if current_length > max_length:  
-            max_length = current_length
-            max_key = key
-print(max_key)
+# max_length = 0
+# max_key = ''
+# for key, value in dict1.items():
+#         current_length = len(value)  
+#         if current_length > max_length:  
+#             max_length = current_length
+#             max_key = key
+# print(max_key)
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = min(a,b,c)
+e = max(a,b,c)
+print(e-d)
