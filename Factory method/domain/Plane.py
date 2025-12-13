55d207e64681c416aec22cec7d7fddbc28253dae
Eshkere-goida <nazarovdaniil05@gmail.com>
2025-12-13 15:48:51 +0300

    Delete Factory method directory

--- Factory method/domain/Plane.py
+++
@@ -1,9 +0,0 @@
-from domain.ITransport import ITransport
-
-class Plane(ITransport):
-    def calculate_cost(distance:float,weight:float,is_express:bool) -> float:
-        price = (distance*50)+(weight*10)
-        if is_express:
-            price+=5000
-            return price
-        return price
